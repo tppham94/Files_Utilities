02ff6b334267a4d90e8f14fa41aa59fdc0ba6436
Tan-Phat Pham <tan-phat.pham@kyndryl.com>
2024-07-09 20:34:50 -0400

    add search_excel function

--- compareChecks.py
+++ compareChecks.py
@@ -10,3 +10,17 @@ def read_text_file(file_path):
                 checkname = line.split("checkname:")[1].strip()
                 checknames.append(checkname)
     return checknames
+
+# Function to search Excel file and extract matches 
+def search_excel(excel_path, checknames):
+    matched_lines = []
+    wb = openpyxl.load_workbook(excel_path)
+    sheet = wb["Tech Spec Main Body"]
+
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        if row[2] in checknames and row[3] in checknames and row[5] in checknames:
+            matched_lines.append(row)
+    
+    wb.close()
+    return matched_lines
+
