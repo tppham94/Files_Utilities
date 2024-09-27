ff8472caab24cc487c8426b45edcd6ad4e40e738
Tan-Phat Pham <tan-phat.pham@kyndryl.com>
2024-09-26 23:33:20 -0400

    correct logic error

--- generateFileListFromFolder.py
+++ generateFileListFromFolder.py
@@ -10,7 +10,7 @@ def write_file_names_with_extension(folder_path, output_file):
 
         # Write file name with extension to an output file
         with open(output_file, 'w') as f:
-            for file_name in file_name:
+            for file_name in file_names:
                 f.write(file_name + '\n')
 
         print(f"File names written to {output_file}")
@@ -18,6 +18,6 @@ def write_file_names_with_extension(folder_path, output_file):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-folder_path = 'C:\SCS\Active Records\Network\9. September 2024\CPPIB\Received 2024'
-output_file = 'file_list.txt'
+folder_path = 'C:/SCS/Active Records/Network/9. September 2024/CPPIB/Received 2024'
+output_file = 'C:/SCS/Active Records/Network/9. September 2024/CPPIB/Received 2024/file_list.txt'
 write_file_names_with_extension(folder_path, output_file)
