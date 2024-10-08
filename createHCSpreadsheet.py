ab73ad11fa97591fff964a60c4a7c928122bebfb
tppham94 <tp.tanphatpham@gmail.com>
2024-10-08 00:50:21 -0400

    Able to read txt file and populate the spreadsheet when generated, but error with an empty sheet added

--- createHCSpreadsheet.py
+++ createHCSpreadsheet.py
@@ -12,20 +12,12 @@ def extractDevicesNames():
     return file_contents
 
 # List of sheet test 
-sheet_names = ['Prod1', 'Prod2', 'Prod3']
-
-# Might not need this one to be double checked 
-def createExcelSpreadsheet():
-    workbook = openpyxl.Workbook()
-    # Select active default sheet 
-    sheet = workbook.active
-    # Save workbook to a file 
-    workbook.save("test.xlsx")
-    print("Excel file created successfully !")
+# sheet_names = ['Prod1', 'Prod2', 'Prod3']
 
 # Creates excel sheet with multiple sheets as needed 
 def generateSheetsFromList():
-    with pd.ExcelWriter('multiple_sheets_example.xlsx', engine='openpyxl') as writer:
+    sheet_names = extractDevicesNames()
+    with pd.ExcelWriter('test.xlsx', engine='openpyxl') as writer:
         for sheet_name in sheet_names:
             # Create a simple DataFrame for each sheet (Can be adjusted)
             df = pd.DataFrame({
@@ -39,4 +31,4 @@ def generateSheetsFromList():
     print("Excel file created with multiple sheets !")
 
 # generateSheetsFromList()
-extractDevicesNames()
+# print(extractDevicesNames())
